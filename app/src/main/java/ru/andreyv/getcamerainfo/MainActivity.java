99698b1ef17cca42f9df02ab75b6aff55ad6aa80
voran@inbox.ru <voran@inbox.ru>
2018-10-08 21:10:28 +0300

    update

--- app/src/main/java/ru/andreyv/getcamerainfo/MainActivity.java
+++ app/src/main/java/ru/andreyv/getcamerainfo/MainActivity.java
@@ -2,13 +2,20 @@ package ru.andreyv.getcamerainfo;
 
 import android.content.Context;
 import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.params.StreamConfigurationMap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private CameraManager mCameraManager = null;
+    private TextView textView;
+    private String[] cameraList = null;
+    private String camera_props = null;
+    CameraCharacteristics cc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +25,27 @@ public class MainActivity extends AppCompatActivity {
         mCameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
 
         try {
-            String[] cameraList = mCameraManager.getCameraIdList();
+            cameraList = mCameraManager.getCameraIdList();
+            for (String CameraID : cameraList) {
+                camera_props = camera_props + "camera " + CameraID + "\n";
+            }
+
         } catch (CameraAccessException e) {
             e.printStackTrace();
         }
+
+        textView = findViewById(R.id.cameraInfo);
+
+        try {
+            cc = mCameraManager.getCameraCharacteristics("0");
+            StreamConfigurationMap configurationMap = cc.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+            System.out.print("");
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+
+
+        textView.setText(camera_props);
 
     }
 
