c574f60ff7e7a966d2af0db681e59faec8a1fc36
Andrey Voronin <andrey.voronin@cd-adapco.com>
2018-10-08 16:12:16 +0300

    Update

--- app/src/main/java/ru/andreyv/getcamerainfo/MainActivity.java
+++ app/src/main/java/ru/andreyv/getcamerainfo/MainActivity.java
@@ -1,13 +1,29 @@
 package ru.andreyv.getcamerainfo;
 
+import android.content.Context;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private CameraManager mCameraManager = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mCameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+
+        try {
+            String[] cameraList = mCameraManager.getCameraIdList();
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+
     }
+
+
 }
